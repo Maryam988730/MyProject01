43f1ff3b32e04e0be179ae8acdd2142361f4061c
Maryam Ahmed <maryamahmed@Maryams-MacBook-Air-2.local>
2025-09-01 20:51:32 +0100

    added signup error messages

--- signup.php
+++ signup.php
@@ -16,6 +16,41 @@ include_once 'header.php'
         </form>
     </div>
 
+    <?php
+
+    # if there exists an error in the url
+    if(isset($_GET["error"])) {
+        # if the error is equal to the given error, then display ther correct message
+        if ($_GET["error"] == "emptyinput") {
+            echo "<p>Fill in all fields!</p>";
+        }
+
+        if ($_GET["error"] == "invalidUid") {
+            echo "<p>This username is invalid!</p>";
+        }
+
+        if ($_GET["error"] == "invalidemail") {
+            echo "<p>This email is invalid!</p>";
+        }
+
+        if ($_GET["error"] == "passworddontmatch") {
+            echo "<p>Passwords dont match!</p>";
+        }
+
+        if ($_GET["error"] == "usertaken") {
+            echo "<p>This user is taken!</p>";
+        }
+
+        if ($_GET["error"] == "none") {
+            echo "<p>Your signed up!</p>";
+        }
+
+
+        
+    }
+
+    ?>
+
 
 </section>
 
